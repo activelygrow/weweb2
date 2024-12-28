067a8990bae079309525aa7c2b1f8092fd525ed6
activelygrow <70472700+activelygrow@users.noreply.github.com>
2024-12-27 21:26:50 -0500

    Update ww-config.js

--- ww-config.js
+++ ww-config.js
@@ -1,16 +1,67 @@
 export default {
   editor: {
     label: {
-      en: "My Element",
+      en: "Inline Elements",
     },
   },
   properties: {
-    textColor: {
+    items: {
       label: {
-        en: "Text color",
+        en: "Items",
+      },
+      type: "Array",
+      itemType: {
+        type: "Object",
+        properties: {
+          text: {
+            label: {
+              en: "Text",
+            },
+            type: "String",
+            defaultValue: "Default text",
+          },
+          type: {
+            label: {
+              en: "Type",
+            },
+            type: "Enum",
+            options: {
+              choices: [
+                { value: "text", label: { en: "Text" } },
+                { value: "link", label: { en: "Link" } },
+                { value: "button", label: { en: "Button" } },
+              ],
+            },
+            defaultValue: "text",
+          },
+        },
+      },
+      defaultValue: [
+        { text: "This is a text", type: "text" },
+        { text: "This is a link", type: "link" },
+        { text: "This is a button", type: "button" },
+      ],
+    },
+    linkColor: {
+      label: {
+        en: "Link Color",
+      },
+      type: "Color",
+      defaultValue: "#007BFF",
+    },
+    buttonBgColor: {
+      label: {
+        en: "Button Background Color",
       },
       type: "Color",
       defaultValue: "#F23636",
+    },
+    buttonTextColor: {
+      label: {
+        en: "Button Text Color",
+      },
+      type: "Color",
+      defaultValue: "#FFFFFF",
     },
   },
 };
